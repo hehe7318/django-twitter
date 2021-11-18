82579c9803372d89bf6a1b7feaa78b830f8286c1
hehe7318 <xuanqihe1999@outlook.com>
2021-11-17 23:23:38 -0800

    Initial likes model and admin

--- tweets/tests.py
+++ tweets/tests.py
@@ -1,15 +1,25 @@
-from django.contrib.auth.models import User
-from django.test import TestCase
-from tweets.models import Tweet
+from testing.testcases import TestCase
 from datetime import timedelta
 from utils.time_helpers import utc_now
 
 
 class TweetTests(TestCase):
+    def setUp(self):
+        self.xuanqi = self.create_user('xuanqi')
+        self.tweet = self.create_tweet(self.xuanqi, content='Hello~I am Xuanqi')
 
     def test_hours_to_now(self):
-        xuanqi = User.objects.create_user(username='xuanqi')
-        tweet = Tweet.objects.create(user=xuanqi, content='Done!')
-        tweet.created_at = utc_now() - timedelta(hours=10)
-        tweet.save()
-        self.assertEqual(tweet.hours_to_now, 10)
+        self.tweet.created_at = utc_now() - timedelta(hours=10)
+        self.tweet.save()
+        self.assertEqual(self.tweet.hours_to_now, 10)
+
+    def test_like_set(self):
+        self.create_like(self.xuanqi, self.tweet)
+        self.assertEqual(self.tweet.like_set.count(), 1)
+
+        self.create_like(self.xuanqi, self.tweet)
+        self.assertEqual(self.tweet.like_set.count(), 1)
+
+        he = self.create_user('he')
+        self.create_like(he, self.tweet)
+        self.assertEqual(self.tweet.like_set.count(), 2)
