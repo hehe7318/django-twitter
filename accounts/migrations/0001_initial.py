1a957ab809db681eb1cd5eca02df45b833f9d80a
hehe7318 <xuanqihe1999@outlook.com>
2021-11-20 19:50:06 -0800

    Initial userprofile model and admin

--- accounts/migrations/0001_initial.py
+++ accounts/migrations/0001_initial.py
@@ -11,22 +11,18 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('tweets', '0001_initial'),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='NewsFeed',
+            name='UserProfile',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('avatar', models.FileField(null=True, upload_to='')),
+                ('nickname', models.CharField(max_length=200, null=True)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('tweet', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to='tweets.tweet')),
-                ('user', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL)),
+                ('updated_at', models.DateTimeField(auto_now=True)),
+                ('user', models.OneToOneField(null=True, on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL)),
             ],
-            options={
-                'ordering': ('user', '-created_at'),
-                'unique_together': {('user', 'tweet')},
-                'index_together': {('user', 'created_at')},
-            },
         ),
     ]
