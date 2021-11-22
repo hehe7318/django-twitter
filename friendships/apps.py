c26e7baddb45339716c056c02551f8ac60d3d705
hehe7318 <xuanqihe1999@outlook.com>
2021-11-21 23:26:37 -0800

    28-newsfeeds-pagination

--- friendships/apps.py
+++ friendships/apps.py
@@ -1,10 +1,5 @@
 from django.apps import AppConfig
 
-认准一手微信study322 其他均为翻录倒卖
-九章来offer都有  需要的+我
-课程目录
-https://www.yuque.com/study001/xk/list
-
 
 class FriendshipsConfig(AppConfig):
     name = 'friendships'
